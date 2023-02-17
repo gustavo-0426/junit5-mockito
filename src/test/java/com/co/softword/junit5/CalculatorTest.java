a795a3a26ce261dba64eb0da06b8a9fccee6cd18
gustavocastro <gustavoprasca@hotmail.com>
2023-02-17 18:06:25 -0500

    agregado verify multiple captor

--- src/test/java/com/co/softword/junit5/CalculatorTest.java
+++ src/test/java/com/co/softword/junit5/CalculatorTest.java
@@ -18,83 +18,89 @@ import com.co.softworld.junit5.Calculator;
 
 class CalculatorTest {
 
-	private static ICalculator calculator;
+    private static ICalculator calculator;
 
-	@BeforeAll
-	static void setUp() {
-		calculator = new Calculator();
-	}
+    @BeforeAll
+    static void setUp() {
+        calculator = new Calculator();
+    }
 
-	@AfterAll
-	static void tearDown() {
-		calculator = null;
-	}
+    @AfterAll
+    static void tearDown() {
+        calculator = null;
+    }
 
-	@Nested
-	class addTest {
-		@Test
-		void AddNotNullTest() {
-			assertNotNull(calculator);
-		}
+    @Nested
+    class addTest {
+        @Test
+        void AddNotNullTest() {
+            assertNotNull(calculator);
+        }
 
-		@Test
-		void AddAssertTest() {
-			assertEquals(30, calculator.add(10, 20));
-		}
-	}
+        @Test
+        void AddAssertTest() {
+            assertEquals(30, calculator.add(10, 20));
+        }
+    }
 
-	@Nested
-	class subtractTest {
-		@Test
-		void subtractAssertTest() {
-			assertEquals(10, calculator.subtract(10, 20));
-		}
-	}
+    @Nested
+    class subtractTest {
+        @Test
+        void subtractAssertTest() {
+            assertEquals(10, calculator.subtract(10, 20));
+        }
+    }
 
-	@Nested // It is used to group several test cases for a method.
-	class divideTest {
+    @Nested // It is used to group several test cases for a method.
+    class divideTest {
 
-		@Test
-		void divideAssertTest() {
-			assertEquals(5, calculator.divide(15, 3));
-		}
-		
-		@Test
-		void divideAssertDoubleRankTest() {
-			assertEquals(3.3, calculator.divide(10, 3), 0.1);
-		}
+        @Test
+        void divideAssertTest() {
+            assertEquals(5, calculator.divide(15, 3));
+        }
 
-		@Test
-		void divideAssertNegativeTest() {
-			assertEquals(2.5, calculator.divide(5, 2));
-		}
+        @Test
+        void divideAssertDoubleRankTest() {
+            assertEquals(3.3, calculator.divide(10, 3), 0.1);
+        }
 
-		@Test
-		void divideAssertExceptionTest() {
-			assertThrows(ArithmeticException.class, () -> calculator.divide(2, 0));
-		}
+        @Test
+        void divideAssertNegativeTest() {
+            assertEquals(2.5, calculator.divide(5, 2));
+        }
 
-		@Disabled("this annotation is used to disable test")
-		@Test
-		void divideAssertExceptionDisableTest() {
-			assertThrows(ArithmeticException.class, () -> calculator.divide(2, 0));
-		}
-	}
+        @Test
+        void divideAssertExceptionTest() {
+            assertThrows(ArithmeticException.class, () -> calculator.divide(2, 0));
+        }
 
-	@ParameterizedTest()
-	@MethodSource("addData")
-	void addParameterizedTest(int num1, int num2, int sum) {
-		assertEquals(sum, calculator.add(num1, num2));
-	}
+        @Disabled("this annotation is used to disable test")
+        @Test
+        void divideAssertExceptionDisableTest() {
+            assertThrows(ArithmeticException.class, () -> calculator.divide(2, 0));
+        }
+    }
 
-	static Stream<Arguments> addData() {
-		return Stream.of(Arguments.of(3, 5, 8), Arguments.of(3, -2, 1), Arguments.of(-3, 2, -1),
-				Arguments.of(-4, -1, -5));
-	}
+    @ParameterizedTest()
+    @MethodSource("addData")
+    void addParameterizedTest(int num1, int num2, int sum) {
+        assertEquals(sum, calculator.add(num1, num2));
+    }
 
-	@Test
-	void longTaskOperationTimeOutTest() {
-		assertTimeout(Duration.ofMillis(2000), () -> calculator.longTaskOperation());
-	}
+    static Stream<Arguments> addData() {
+        return Stream.of(Arguments.of(3, 5, 8), Arguments.of(3, -2, 1), Arguments.of(-3, 2, -1),
+                Arguments.of(-4, -1, -5));
+    }
+
+    @Test
+    void longTaskOperationTimeOutTest() {
+        assertTimeout(Duration.ofMillis(2000), () -> calculator.longTaskOperation());
+    }
+
+    @Test
+    void argumentCaptorTest() {
+        calculator.divide(15, 5);
+
+    }
 
 }
