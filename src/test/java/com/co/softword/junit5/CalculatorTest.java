b300024858fb67f543ba580da427f779725daa6f
Gustavo Castro Prasca <gcastro@novopayment.com>
2023-05-10 07:15:50 -0500

    Se agrega la etiqueta tag para algunas pruebas

--- src/test/java/com/co/softword/junit5/CalculatorTest.java
+++ src/test/java/com/co/softword/junit5/CalculatorTest.java
@@ -51,7 +51,8 @@ class CalculatorTest {
         }
     }
 
-    @Nested // It is used to group several test cases for a method.
+    @Nested
+    @Tag("divide")
     class divideTest {
 
         @Test
@@ -81,6 +82,7 @@ class CalculatorTest {
         }
     }
 
+    @Tag("add")
     @ParameterizedTest
     @ValueSource(ints = {2, 4, 6})
     void addValueSourceParameterizedTest(int num1) {
@@ -88,24 +90,29 @@ class CalculatorTest {
         int num2 = sum - num1;
         assertEquals(sum, calculator.add(num1, num2));
     }
+
+    @Tag("add")
     @ParameterizedTest
     @CsvSource({"3,5,8", "3,-2,1", "-3,2,-1", "-4,-1,-5"})
     void addCsvSourceParameterizedTest (int num1, int num2, int sum) {
         assertEquals(sum, calculator.add(num1, num2));
     }
 
+    @Tag("add")
     @ParameterizedTest
     @CsvSource(value = {"3:5:8", "3:-2:1", "-3:2:-1", "-4:-1:-5"}, delimiter = ':')
     void addCsvSourceDelimiterParameterizedTest (int num1, int num2, int sum) {
         assertEquals(sum, calculator.add(num1, num2));
     }
 
+    @Tag("add")
     @ParameterizedTest
     @CsvFileSource(resources = {"/CsvFileTest.csv"})
     void addCsvDelimiterParameterizedTest (int num1, int num2, int sum) {
         assertEquals(sum, calculator.add(num1, num2));
     }
 
+    @Tag("add")
     @ParameterizedTest()
     @MethodSource("addData")
     void addMethodSourceParameterizedTest(int num1, int num2, int sum) {
@@ -135,8 +142,27 @@ class CalculatorTest {
     }
 
     @Test
+    @Tag("divide")
     void argumentCaptorTest() {
         assertEquals(3, calculator.divide(15, 5));
+    }
+
+    @Test
+    @Tag("divide")
+    void tagDivideTest() {
+        assertEquals(10, calculator.divide(20, 2));
+    }
+
+    @Test
+    @Tag("divide")
+    void tagDivide2Test() {
+        assertEquals(12, calculator.divide(48, 4));
+    }
+
+    @Test
+    @Tag("add")
+    void tagAddTest() {
+        assertEquals(8, calculator.add(5, 3));
     }
 
 }
